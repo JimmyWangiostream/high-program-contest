8a61153785862f7d846af77aff0d145ece4f4a58
JimmyWangiostream <jimmy85117@gmail.com>
2020-03-14 22:25:00 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+
 #define endl '\n'
 
 using namespace std;
 
-int main () {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    /*implement your code*/
-    
-    
-    
-    
-    /*implement your code*/
-    return 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	/*implement your code*/
+	
+	
+
+
+	/*implement your code*/
+	return 0;
 }
