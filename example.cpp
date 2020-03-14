b80c92dad1ee39cd414c492571d1c7b388abef8c
JimmyWangiostream <jimmy85117@gmail.com>
2020-03-14 13:18:36 +0800

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*implement your code*/
+    
+    
+    
+    
+    /*implement your code*/
+    return 0;
+}
