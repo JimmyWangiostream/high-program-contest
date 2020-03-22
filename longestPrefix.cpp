135d7a2f4562eaadfca5172b0b912fd9c0c4a77a
JimmyWangiostream <jimmy85117@gmail.com>
2020-03-22 23:07:59 +0800

    Update longestPrefix.cpp

--- longestPrefix.cpp
+++ longestPrefix.cpp
@@ -7,3 +7,50 @@ public:
     return ""; // needed to compile (but never hit)
 }
 };
+
+
+//lps
+int longestPrefixSuffix(string s) 
+{ 
+    int n = s.length(); 
+  
+    int lps[n]; 
+    lps[0] = 0; // lps[0] is always 0 
+
+    int len = 0; 
+  
+    int i = 1; 
+    while (i < n) 
+    { 
+        if (s[i] == s[len]) 
+        { 
+            len++; 
+            lps[i] = len; 
+            i++; 
+        } 
+        else 
+        { 
+            if (len != 0) 
+            { 
+                len = lps[len-1]; 
+            } 
+            else // if (len == 0) 
+            { 
+                lps[i] = 0; 
+                i++; 
+            } 
+        } 
+    } 
+  
+    int res = lps[n-1]; 
+  
+    return (res > n/2)? n/2 : res; 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+    string s = "abcab"; 
+    cout << longestPrefixSuffix(s); 
+    return 0; 
+} 
